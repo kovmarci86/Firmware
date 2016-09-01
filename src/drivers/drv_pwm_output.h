9b68d935cfb9355a8aeea79083104b7ea58a798f
Marcell Kovacs <marcell_kovacs@epam.com>
2016-09-01 12:34:54 +0200

    Add 5 PWM constant to PWM output driver.

--- src/drivers/drv_pwm_output.h
+++ src/drivers/drv_pwm_output.h
@@ -251,11 +251,12 @@ struct pwm_output_rc_config {
 #define PWM_SERVO_MODE_3PWM			3
 #define PWM_SERVO_MODE_3PWM1CAP			4
 #define PWM_SERVO_MODE_4PWM			5
-#define PWM_SERVO_MODE_6PWM			6
-#define PWM_SERVO_MODE_8PWM			7
-#define PWM_SERVO_MODE_4CAP			8
-#define PWM_SERVO_MODE_5CAP			9
-#define PWM_SERVO_MODE_6CAP		       10
+#define PWM_SERVO_MODE_5PWM			6
+#define PWM_SERVO_MODE_6PWM			7
+#define PWM_SERVO_MODE_8PWM			8
+#define PWM_SERVO_MODE_4CAP			9
+#define PWM_SERVO_MODE_5CAP			10
+#define PWM_SERVO_MODE_6CAP		       11
 #define PWM_SERVO_SET_MODE			_PX4_IOC(_PWM_SERVO_BASE, 32)
 
 /*
